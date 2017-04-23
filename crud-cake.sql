a0e44abc764303b5fa3442de096b22f7556affd2
Marcio Gimenez <marciogimenez1093@gmail.com>
2017-04-23 17:40:31 -0300

    Sql

---
+++ crud-cake.sql
@@ -0,0 +1,30 @@
+--
+-- Database: `crud-cake`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
